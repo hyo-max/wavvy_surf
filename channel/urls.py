553e8e6d3efe7fbc3e1ad8982adf27ac7d131f46
hyo-max <yula1147@yahoo.com>
2022-03-11 00:51:46 +0900

    restart

--- channel/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('channel_list/',views.channel_list, name='channel_list'),
-    path('channel_detail/<int:video_id>',views.channel_detail, name='channel_detail'),
-    ]
--- school/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def school_list(request):
-    return render(request, 'school/school_list.html')
-
-
-def school_detail(request):
-    return render(request, 'school/school_detail.html')
